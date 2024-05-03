dc7e2d3d4d35a7780c87441da3dc2e7fca0e0871
Rohit Anil Shinde <122345803+rohitshinde3903@users.noreply.github.com>
2024-05-03 19:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,48 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 import random
 
 app = Flask(__name__)
 
 def initialize_game():
-    session['computer'] = random.randint(1, 100)
-    session['counter'] = 0
-    session['guessed_numbers'] = []
+    return {
+        'computer': random.randint(1, 100),
+        'counter': 0,
+        'guessed_numbers': []
+    }
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'computer' not in session:
-        initialize_game()
+    if 'game' not in session:
+        session['game'] = initialize_game()
+
+    game = session['game']
 
     message = ""
     result = False
     retain_focus = False  # Flag to retain focus on input field
     if request.method == 'POST':
         guess = int(request.form['guess'])
-        session['counter'] += 1
-        session['guessed_numbers'].append(guess)
+        game['counter'] += 1
+        game['guessed_numbers'].append(guess)
 
-        if guess != session['computer']:
-            if guess > session['computer']:
+        if guess != game['computer']:
+            if guess > game['computer']:
                 message = "Guess lower"
             else:
                 message = "Guess higher"
             retain_focus = True  # Set the flag to retain focus on input field
         else:
-            message = "Correct guess! You guessed it in {} attempts.".format(session['counter'])
+            message = "Correct guess! You guessed it in {} attempts.".format(game['counter'])
             result = True
 
-    return render_template('index.html', message=message, result=result, counter=session.get('counter', 0),
-                           guessed_numbers=session.get('guessed_numbers', []), retain_focus=retain_focus)
+    return render_template('index.html', message=message, result=result, counter=game['counter'],
+                           guessed_numbers=game['guessed_numbers'], retain_focus=retain_focus)
 
 @app.route('/reset')
 def reset():
-    initialize_game()
+    session.pop('game', None)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.secret_key = '1'
+    app.secret_key='1'
     app.run(debug=True)
